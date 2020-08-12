4cc2b9b1098184636b2bf20e48f77af6f3f612ca
David Molina <davidmolina2810@gmail.com>
2020-08-12 16:00:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-#1. Use .split(" ") to turn str into array and assign to arr 
+#1. Use .split(" ") to turn str into array and assign to arr
 #2. Create empty array to hold reversed strings and assign to reverse_arr
 #3. Iterate over arr with .each
-  #4. Reverse each string element in arr 
+  #4. Reverse each string element in arr
   #5. reverse_arr << reversed string element
 #6. reversed_str = reverse_arr.join(" ")
 #7. return reversed_str
@@ -16,9 +16,6 @@ def reverse_each_word(str)
   reversed_str
 end
 
-#1. Use .split(" ") to turn str into array and assign to arr 
+#1. Use .split(" ") to turn str into array and assign to arr
 #2. Use .collect! on arr and return array with each string reversed and assign to reverse_arr
 #3. Use .join(" ") to join reverse_arr into str and return 
-
-
-  
